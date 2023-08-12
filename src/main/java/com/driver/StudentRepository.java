7e2effbdbef2c3c67801767b4c0810c282dd3f84
Ashwini S Biradar <ashwinibiradar33@gmail.com>
2023-08-12 23:11:29 +0530

    Ashwini Updated

--- src/main/java/com/driver/StudentRepository.java
+++ src/main/java/com/driver/StudentRepository.java
@@ -1,13 +1,68 @@
 package com.driver;
+import java.util.*;
+public class StudentRepository {
 
-import java.util.List;
+    HashMap<String, Student> studentdb = new HashMap<>();
+    HashMap<String, Teacher> teacherdb = new HashMap<>();
+    HashMap<String, List<String>>  studentteacherPair = new HashMap<>();
 
-public interface StudentRepository {
-    void addStudent(Student student);
+    public void addStudentdb(Student student)
+    {
+        String name=student.getName();
 
-    Student getStudentByName(String name);
+        studentdb.put(name, student);
+    }
 
-    List<Student> getAllStudents();
+    public void addTeacherdb(Teacher teacher)
+    {
+        String name=teacher.getName();
 
-    void deleteAllStudents();
+        teacherdb.put(name, teacher);
+    }
+
+    public void addpair(String student, String teacher)
+    {
+        List<String> s =studentteacherPair.getOrDefault(teacher, new ArrayList<>());
+        s.add(student);
+
+    }
+
+    public Student getSt(String  s)
+    {
+
+        return studentdb.get(s);
+    }
+
+    public Teacher getT(String s)
+    {
+        return teacherdb.get(s);
+    }
+
+    public List<String> getStudentsAssigned(String teacher)
+    {
+        return studentteacherPair.get(teacher);
+    }
+
+    public List<String> getStudents()
+    {
+        List<String> st = new ArrayList<>();
+
+        for(String name : studentdb.keySet())
+            st.add(name);
+        return st;
+    }
+
+    public void deleteTe(String name)
+    {
+        if(teacherdb.containsKey(name))
+            teacherdb.remove(name);
+
+
+    }
+
+    public void deleteAllteachers()
+    {
+        teacherdb.clear();
+
+    }
 }
--- src/main/java/com/driver/StudentService.java
+++ src/main/java/com/driver/StudentService.java
@@ -1,18 +1,51 @@
 package com.driver;
 
-
-import org.springframework.http.ResponseEntity;
-
 import java.util.List;
 
-public interface StudentService {
-        ResponseEntity<String> addStudent(Student student);
+public class StudentService {
 
-        ResponseEntity<Student> getStudentByName(String name);
+    StudentRepository studentRepository = new StudentRepository();
+    public void addStudents(Student student)
+    {
+        studentRepository.addStudentdb(student);
 
-        ResponseEntity<List<String>> getAllStudents();
-
-        ResponseEntity<String> deleteAllStudents();
     }
 
+    public void addTeacher(Teacher teacher)
+    {
+        studentRepository.addTeacherdb(teacher);
+    }
+    public void addTeacherStudentPair(String student, String teacher)
+    {
+        studentRepository.addpair(student, teacher);
+    }
 
+    public Student getStudentByname(String name)
+    {
+        return studentRepository.getSt(name);
+    }
+    public Teacher findt(String name)
+    {
+        return studentRepository.getT(name);
+    }
+
+    public List<String> getStudentsAssignedToTeacher(String name)
+    {
+        return studentRepository.getStudentsAssigned(name);
+    }
+
+    public List<String> getAllStudentsfromService()
+    {
+        return studentRepository.getStudents();
+    }
+
+    public void deleteTeacher(String name)
+    {
+        studentRepository.deleteTe(name);
+    }
+
+    public void deleteAll()
+    {
+        studentRepository.deleteAllteachers();
+    }
+}
